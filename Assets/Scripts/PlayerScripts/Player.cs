2d5f099cd5df8f5d74c2cab49b15253bcd72ba45
Marco Caponera <marco.caponera4@gmail.com>
2024-05-31 11:35:39 +0200

    player base
    added:
    - player script
    - player prefab base

---
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GrappleZ_Player
+{
+    public class Player : MonoBehaviour
+    {
+        #region StaticMembers
+        //player instance
+        public static Player instance; 
+
+        //returns instance if instance has already been assigned
+        public static Player Get()
+        {
+            if (instance != null) return instance;
+            //finds player and returns it if instance is null
+            instance = FindObjectOfType<Player>();
+            return instance;
+        }
+        #endregion
+
+        #region Mono
+
+        private void Awake()
+        {
+            //singleton pattern, ensures that there will be only one instance of the player
+            //if a player already exists, the gameObject will destroy itself
+            if (instance != null && instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        #endregion
+    }
+}
