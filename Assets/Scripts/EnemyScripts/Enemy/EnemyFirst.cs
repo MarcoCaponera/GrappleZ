6543778010edb44d9b559fda2bf3b9e7e0c3ba8d
Zigghi7 <moramarco44@gmail.com>
2024-06-28 12:58:44 +0200

    Audio
    to fix

--- Assets/Scripts/EnemyScripts/Enemy/EnemyFirst.cs
+++ Assets/Scripts/EnemyScripts/Enemy/EnemyFirst.cs
@@ -3,8 +3,9 @@ using UnityEngine;
 using UnityEngine.Pool;
 using GrappleZ_Player;
 using UnityEngine.AI;
-using static UnityEngine.GraphicsBuffer;
 using System.Collections.Generic;
+using System.Collections;
+using Codice.Client.BaseCommands;
 
 
 
@@ -48,23 +49,45 @@ public class EnemyFirst : MonoBehaviour, IDamager, IDamageble
 
     private EnemySpawnerBase spawnController;
     [SerializeField]
-    private AudioSource audioSource;
+    protected AudioSource audioSource;
     [SerializeField]
-    private AudioClip[] attackClipList;
+    private AudioClip deathClip;
+    [SerializeField]
+    private AudioClip attackClipList1;
+    [SerializeField]
+    private AudioClip attackClipList2;
+    [SerializeField]
+    private AudioClip attackClipList3;
 
 
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.speed = enemySpeed;
-        agent.stoppingDistance = attackingRadius+2;
+        agent.stoppingDistance = attackingRadius + 2;
 
         anim = GetComponent<Animator>();
         currentHealt = healt;
         player = Player.Get().transform;
         lookPoint = Player.Get().transform;
 
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+
+
     }
+
+    #region Audio Methods
+    private void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+        {
+
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
+    #endregion
 
     public void Initialize()
     {
@@ -80,7 +103,7 @@ public class EnemyFirst : MonoBehaviour, IDamager, IDamageble
     {
         gameObject.SetActive(true);
     }
- 
+
 
     protected virtual void Die()
     {
@@ -105,12 +128,14 @@ public class EnemyFirst : MonoBehaviour, IDamager, IDamageble
     protected virtual void Attack()
     {
 
+        PlaySound(attackClipList2);
         transform.LookAt(lookPoint);
 
         if (!hasAttacked)
         {
+
             RaycastHit hitInfo;
-            if (Physics.Raycast(AttackingRaycastArea.transform.position, AttackingRaycastArea.transform.forward, out hitInfo, attackingRadius)) 
+            if (Physics.Raycast(AttackingRaycastArea.transform.position, AttackingRaycastArea.transform.forward, out hitInfo, attackingRadius))
             {
                 Debug.Log("Attacking" + hitInfo.transform.name);
                 if (!hitInfo.collider.CompareTag("Player")) return;
@@ -120,12 +145,10 @@ public class EnemyFirst : MonoBehaviour, IDamager, IDamageble
                 {
                     DamageContainer damageContainer = new DamageContainer();
                     damageContainer.Damage = damage;
-                    
+
                     damageble.TakeDamage(damageContainer);
 
                 }
-                
-
             }
 
             anim.SetBool("Walking", false);
@@ -133,7 +156,7 @@ public class EnemyFirst : MonoBehaviour, IDamager, IDamageble
             anim.SetBool("Attacking", true);
             anim.SetBool("Dead", false);
 
-            audioSource.PlayOneShot(attackClipList[UnityEngine.Random.Range(0, attackClipList.Length-1)]);
+
 
             hasAttacked = true;
             Invoke(nameof(ActiveAttacking), timeBetweenAttack);
@@ -157,8 +180,9 @@ public class EnemyFirst : MonoBehaviour, IDamager, IDamageble
         if (player.position.y > 1)
         {
             agent.SetDestination(new Vector3(player.position.x, 1, player.position.z));
-            
-        }else
+
+        }
+        else
         {
 
             SetAgentDestination();
@@ -177,7 +201,7 @@ public class EnemyFirst : MonoBehaviour, IDamager, IDamageble
             anim.SetBool("Attacking", false);
             anim.SetBool("Dead", false);
         }
-        
+
     }
 
     private bool SetAgentDestination()
@@ -203,21 +227,28 @@ public class EnemyFirst : MonoBehaviour, IDamager, IDamageble
             anim.SetBool("Idle", false);
             anim.SetBool("Attacking", false);
             anim.SetBool("Dead", true);
+
+            PlaySound(deathClip);
+
+           
             Die();
         }
     }
+    
+  
 
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            InternalTakeDamage(damage);
-        }
+        //if (other.CompareTag("Player"))
+        //{
+        //    InternalTakeDamage(damage);
+        //}
     }
 
     public void TakeDamage(DamageContainer damage)
     {
         InternalTakeDamage(damage.Damage);
     }
+    
 }
