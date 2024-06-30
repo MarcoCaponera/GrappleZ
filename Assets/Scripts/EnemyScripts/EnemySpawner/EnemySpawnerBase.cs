1d9549edd03aba337599d2e5bdf49f7b30b16caf
Zigghi7 <moramarco44@gmail.com>
2024-07-01 01:09:09 +0200

    Fix commit
    To end spawner

--- Assets/Scripts/EnemyScripts/EnemySpawner/EnemySpawnerBase.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner/EnemySpawnerBase.cs
@@ -1,159 +1,5 @@
-<<<<<<< Updated upstream
+
 using GrappleZ_Utility;
-=======
-
-#region OldScript
-//using PlasticGui.WorkspaceWindow;
-//using System;
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.Pool;
-
-//public enum EnemyType
-//{
-//    Walker,
-//    Sniper,
-//    Runner
-//}
-
-
-//    public class EnemySpawnerBase : MonoBehaviour
-//    {
-//        [Serializable]
-//        public class Pool
-//        {
-//            public EnemyType Type;
-//            public GameObject Prefab;
-//            public int Size;
-//        }
-
-//        [Serializable]
-//        public class Wave
-//        {
-//            public List<EnemyType> EnemyTypes;
-//            public int TotalEnemiesInWave;
-//            public float SpawnInterval;
-//        }
-
-//        public List<Pool> Pools;
-//        public List<Wave> Waves;
-//        private Dictionary<EnemyType, Queue<GameObject>> poolDictionary;
-
-//        [SerializeField]
-//        private Pool pool;
-
-//        [SerializeField]
-//        private int totalEnemiesToSpawn;
-//        [SerializeField]
-//        private float spawnInterval;
-//        [SerializeField]
-//        private Vector3 spawnPoint;
-
-//        private static int enemiesSpawned;
-//        private static int counter;
-
-//        void Start()
-//        {
-//            poolDictionary = new Dictionary<EnemyType, Queue<GameObject>>();
-
-//            foreach (Pool pool in Pools)
-//            {
-//                Queue<GameObject> objectPool = new Queue<GameObject>();
-
-//                for (int i = 0; i < pool.Size; i++)
-//                {
-//                    GameObject obj = Instantiate(pool.Prefab);
-//                    obj.SetActive(false);
-//                    objectPool.Enqueue(obj);
-//                }
-
-//                poolDictionary.Add(pool.Type, objectPool);
-//            }
-
-
-//            enemiesSpawned = 0;
-//            GlobalEventManager.CastEvent(GlobalEventIndex.WaveStarted, GlobalEventArgsFactory.WaveStartedFactory());
-//            StartCoroutine(SpawnEnemies());
-//        }
-
-//        IEnumerator SpawnEnemies()
-//        {
-//            while (enemiesSpawned < totalEnemiesToSpawn)
-//            {
-//                yield return new WaitForSeconds(spawnInterval);
-
-//                EnemyType randomType = GetRandomEnemyType();
-//                Vector3 spawnPosition = GetRandomSpawnPosition();
-//                SpawnFromPool(randomType, spawnPosition, Quaternion.identity);
-
-//                enemiesSpawned++;
-//            }
-
-
-//        }
-
-//        private EnemyType GetRandomEnemyType()
-//        {
-//            //int randomIndex = pools.Count;
-//            int randomIndex = UnityEngine.Random.Range(0, Pools.Count);
-
-
-//            return Pools[randomIndex].Type;
-//        }
-
-//        public GameObject SpawnFromPool(EnemyType type, Vector3 position, Quaternion rotation)
-//        {
-//            if (!poolDictionary.ContainsKey(type))
-//            {
-//                Debug.LogWarning("Pool with type " + type + " doesn't exist.");
-//                return null;
-//            }
-
-//            GameObject objectToSpawn = poolDictionary[type].Dequeue();
-
-//            objectToSpawn.SetActive(true);
-//            objectToSpawn.transform.position = position;
-//            objectToSpawn.transform.rotation = rotation;
-
-//            EnemyFirst enemy = objectToSpawn.GetComponent<EnemyFirst>();
-
-//            enemy.OnSpawn();
-
-//            poolDictionary[type].Enqueue(objectToSpawn);
-
-//            return objectToSpawn;
-//        }
-
-//        public void DespawnToPool(GameObject objectToDespawn)
-//        {
-//            objectToDespawn.SetActive(false);
-
-//            EnemyFirst enemy = objectToDespawn.GetComponent<EnemyFirst>();
-//            enemy.OnDespawn();
-//            counter++;
-//            CheckForWaveEnded();
-
-//        }
-
-//        private void CheckForWaveEnded()
-//        {
-//            if (counter >= totalEnemiesToSpawn)
-//            {
-//                GlobalEventManager.CastEvent(GlobalEventIndex.WaveEnded, GlobalEventArgsFactory.WaveEndedFactory());
-//            }
-//    }
-
-//        private Vector3 GetRandomSpawnPosition()
-//        {
-//            //int randomIndex = Random.Range(0, spawnPoints.Count);
-//            return spawnPoint;
-//        }
-//    }
-
-#endregion
-
->>>>>>> Stashed changes
 using PlasticGui.WorkspaceWindow;
 using System;
 using System.Collections;
@@ -228,93 +74,16 @@ public class EnemySpawnerBase : MonoBehaviour
         if (currentWaveIndex < Waves.Count)
         {
             enemiesSpawned = 0;
-<<<<<<< Updated upstream
-            GlobalEventManager.CastEvent(GlobalEventIndex.WaveStarted, GlobalEventArgsFactory.WaveStartedFactory(WaveEnum.First));
-            StartCoroutine(SpawnEnemies());
-=======
             enemiesDefeated = 0;
             Wave currentWave = Waves[currentWaveIndex];
-            GlobalEventManager.CastEvent(GlobalEventIndex.WaveStarted, GlobalEventArgsFactory.WaveStartedFactory());
+            //GlobalEventManager.CastEvent(GlobalEventIndex.WaveStarted, GlobalEventArgsFactory.WaveStartedFactory());
             StartCoroutine(SpawnEnemiesForWave(currentWave));
->>>>>>> Stashed changes
         }
         else
         {
-<<<<<<< Updated upstream
-            while (enemiesSpawned < totalEnemiesToSpawn)
-            {
-                yield return new WaitForSeconds(spawnInterval);
-
-                EnemyType randomType = GetRandomEnemyType();
-                Vector3 spawnPosition = GetRandomSpawnPosition();
-                SpawnFromPool(randomType, spawnPosition, Quaternion.identity);
-
-                enemiesSpawned++;
-            }
             
-
-        }
-
-        private EnemyType GetRandomEnemyType()
-        {
-            //int randomIndex = pools.Count;
-            int randomIndex = UnityEngine.Random.Range(0, pools.Count);
-
-
-            return pools[randomIndex].type;
-        }
-
-        public GameObject SpawnFromPool(EnemyType type, Vector3 position, Quaternion rotation)
-        {
-            if (!poolDictionary.ContainsKey(type))
-            {
-                Debug.LogWarning("Pool with type " + type + " doesn't exist.");
-                return null;
-            }
-
-            GameObject objectToSpawn = poolDictionary[type].Dequeue();
-
-            objectToSpawn.SetActive(true);
-            objectToSpawn.transform.position = position;
-            objectToSpawn.transform.rotation = rotation;
-
-            EnemyFirst enemy = objectToSpawn.GetComponent<EnemyFirst>();
-
-            enemy.OnSpawn();
-
-            poolDictionary[type].Enqueue(objectToSpawn);
-
-            return objectToSpawn;
-        }
-
-        public void DespawnToPool(GameObject objectToDespawn)
-        {
-            objectToDespawn.SetActive(false);
-
-            EnemyFirst enemy = objectToDespawn.GetComponent<EnemyFirst>();
-            enemy.OnDespawn();
-            counter++;
-            CheckForWaveEnded();
+            Debug.Log("All waves completed");
             
-        }
-
-        private void CheckForWaveEnded()
-        {
-            if (counter >= totalEnemiesToSpawn)
-            {
-                GlobalEventManager.CastEvent(GlobalEventIndex.WaveEnded, GlobalEventArgsFactory.WaveEndedFactory(WaveEnum.First));
-            }
-    }
-
-        private Vector3 GetRandomSpawnPosition()
-        {
-            //int randomIndex = Random.Range(0, spawnPoints.Count);
-            return spawnPoint;
-=======
-            // All waves completed
-            Debug.Log("All waves completed!");
-            // victory or defeat
->>>>>>> Stashed changes
         }
     }
 
@@ -331,11 +100,11 @@ public class EnemySpawnerBase : MonoBehaviour
         }
     }
 
-    public GameObject SpawnFromPool(EnemyType type, Vector3 position, Quaternion rotation)
+    private GameObject SpawnFromPool(EnemyType type, Vector3 position, Quaternion rotation)
     {
         if (!poolDictionary.ContainsKey(type))
         {
-            Debug.LogWarning("Pool with type " + type + " doesn't exist.");
+            Debug.LogWarning("Pool with type " + type + " doesn't exist");
             return null;
         }
 
@@ -368,7 +137,7 @@ public class EnemySpawnerBase : MonoBehaviour
     {
         if (enemiesDefeated >= Waves[currentWaveIndex].TotalEnemiesInWave)
         {
-            GlobalEventManager.CastEvent(GlobalEventIndex.WaveEnded, GlobalEventArgsFactory.WaveEndedFactory());
+            //GlobalEventManager.CastEvent(GlobalEventIndex.WaveEnded, GlobalEventArgsFactory.WaveEndedFactory());
             StartNextWave();
         }
     }
